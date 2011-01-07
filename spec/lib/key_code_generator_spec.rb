022b18c341ae43a626d94109620fbbbdf1dcfb08
Arjun Anand <arjun.anand84@gmail.com>
2011-01-06 22:46:39 -0500

    add benchmarking. remove swp files

--- spec/lib/key_code_generator_spec.rb
+++ spec/lib/key_code_generator_spec.rb
@@ -3,24 +3,34 @@ require 'lib/key_code_generator'
 
 describe KeyCodeGenerator, ".generate" do
   it "returns an array" do
-    KeyCodeGenerator.generate.should be_a_kind_of(Array)
+    @reports << Benchmark.measure do
+      KeyCodeGenerator.generate.should be_a_kind_of(Array)
+    end
   end
 
   it "generates a key of length 6" do
-    KeyCodeGenerator.generate.map { |key_code| key_code.length.should == 6}
+    @reports << Benchmark.measure do
+      KeyCodeGenerator.generate.map { |key_code| key_code.length.should == 6}
+    end
   end
 
   it "doesnt include 0,1,2,o,l,z" do
     excludes = [0, 1, 2, "o", "l", "z"]
-    key_codes = KeyCodeGenerator.generate
-    (key_codes & excludes).should be_empty
+    @reports << Benchmark.measure do
+      key_codes = KeyCodeGenerator.generate
+      (key_codes & excludes).should be_empty
+    end
   end
 
   it "returns an array of length 5000" do
-    KeyCodeGenerator.generate.length.should == 5000
+    @reports << Benchmark.measure do
+      KeyCodeGenerator.generate.length.should == 5000
+    end
   end
 
   it "returns all unique keys" do
-    KeyCodeGenerator.generate.uniq!.should be_nil
+    @reports << Benchmark.measure do
+      KeyCodeGenerator.generate.uniq!.should be_nil
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
 require 'rspec'
+require 'benchmark'
 require 'shoulda'
 require 'shoulda/integrations/rspec'
+Spec::Runner.configure do |config|
+
+  config.before(:all) do
+    @reports = []
+  end
+  config.after(:all) do
+    @reports.each do |report|
+      puts ''
+      puts report
+    end
+  end
+end
